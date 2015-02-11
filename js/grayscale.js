2c1c49803be1372a3e79da7e1ebba9f26a388d70
thecristen <cristen@outlook.com>
2015-02-11 12:10:27 -0500

    updated in response to feedback

--- js/grayscale.js
+++ js/grayscale.js
@@ -33,3 +33,60 @@ $('.speaker').click(function(e) {
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// Google Maps Scripts
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 16,
+
+        // The latitude and longitude to center the map (always required)
+        center: new google.maps.LatLng(42.361807, -71.090280), // MIT Stata
+
+        // Disables the default Google Maps UI components
+        disableDefaultUI: true,
+        scrollwheel: false,
+        draggable: false,
+
+        // How you would like to style the map. 
+        // This is where you would paste any style found on Snazzy Maps.
+        styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using out element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+    var image = 'img/map-marker.png';
+    var myLatLng = new google.maps.LatLng(42.361807, -71.090280);
+    var venueMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
+
+    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+    var parking = 'img/parking-marker.png';
+    var parkingLatLng = new google.maps.LatLng(42.36135, -71.0856);
+    var parkingMarker = new google.maps.Marker({
+        position: parkingLatLng,
+        map: map,
+        icon: parking
+    });
+
+    //Info for the location
+    var locationInfo = 'Stata Center, Room 32-155';
+    var infowindow = new google.maps.InfoWindow({
+      content: locationInfo
+    });
+    infowindow.open(map,venueMarker);
+}
